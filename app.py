51890d724d8573b8265bd475004a9cd38d9f91bb
Raju Meher <rajumeher@Rajus-MacBook-Air.local>
2025-02-14 22:04:42 +0530

    Web app ready

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
-def predict():
+def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -22,6 +22,13 @@ def predict():
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    output = regmodel.predict(final_input)
+    return render_template('home.html', prediction_text="The predicted House Price is {}".format(output[0]))
 
 
 if __name__ == "__main__":
